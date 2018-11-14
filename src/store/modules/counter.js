8b7857f0df6a8fae8afc8baebefde6a5d5cb080f
김재근 <jkkim@comtrue.com>
2018-11-14 15:25:33 +0900

    redux-actions 의 createAction 과 handleActions 사용

--- src/store/modules/counter.js
+++ src/store/modules/counter.js
@@ -1,4 +1,10 @@
-// 카운터 관련 상태 로직
+/* 
+ 카운터 관련 상태 로직
+ 리덕스 메뉴얼상 액션과 리듀서를 각각 다른 파일에 작성하여 관리하는 것을 알려주지만, 새 액션을 추가 할때마다 두 파일을 건들어야 함
+ 액션 + 리듀서 -> Ducks 구조
+ 기능별로 하나의 파일에서 관리 가능
+*/
+import { createAction, handleActions } from "redux-actions";
 
 // Action type
 const INCREMENT = "counter/INCREMENT";
@@ -6,21 +12,22 @@ const DECREMENT = "counter/DECREMENT";
 
 // Action functions
 // 액션 함수의 경우 다른 파일에서 호출되기 때문에 export 해줌
-export const increment = () => ({ type: INCREMENT });
-export const decrement = () => ({ type: DECREMENT });
+export const increment = createAction(INCREMENT);
+export const decrement = createAction(DECREMENT);
 
 const initialState = {
   number: 0
 };
 
 // Reducer를 만들어 export
-export default function reducer(state = initialState, action) {
-  switch (action.type) {
-    case INCREMENT:
+export default handleActions(
+  {
+    [INCREMENT]: (state, action) => {
       return { number: state.number + 1 };
-    case DECREMENT:
-      return { nuber: state.number - 1 };
-    default:
-      return state;
-  }
-}
+    },
+    [DECREMENT]: (state, action) => {
+      return { number: state.number - 1 };
+    }
+  },
+  initialState
+);
